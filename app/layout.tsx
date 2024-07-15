67e8aa7d77e479d123d95e08b1f8fbe1ace98c8e
Kosuke Takahashi <rhcp.kdog@gmail.com>
2024-07-15 22:36:33 +0900

    complete section 5.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,3 +12,5 @@ export default function RootLayout({
     </html>
   )
 }
+
+// By having a special name for page files, Next.js allows you to colocate UI components, test files, and other related code with your routes. Only the content inside the page file will be publicly accessible. For example, the /ui and /lib folders are colocated inside the /app folder along with your routes.
